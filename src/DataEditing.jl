cfb6aa9ad3b6ea5d26ad40c71adea4c67e67e0d9
Graham Stark <graham.stark@virtual-worlds.biz>
2020-03-06 20:31:52 +0000

    year changes

--- src/DataEditing.jl
+++ src/DataEditing.jl
@@ -12,27 +12,47 @@ module DataEditing
     using GlobalDecls
     using Utils
 
-    export makeediteddatasets, addonscodesto!,makeditedhbdata
+    export make_edited_datasets, add_ons_codes_to!,make_edited_hb_data
+
+    function yearstr( year :: Integer ) :: String
+        ys = "?"
+        if year == 2018
+            ys = "2017-18"
+        elseif year == 2019
+            ys = "2018-19"
+        end
+        @assert ys != "?"
+        ys
+    end
+
+    """
+    FIXME we only actually have 1 set of this data
+    """
+    function get_payment_data( year :: Integer )::DataFrame
+        CSV.File(
+            DATADIR*"Staying_Put_grant_letter_to_LAs_2019-20_220319.csv",
+            delim=',' ) |> DataFrame
+    end
 
     """
     This is the LA payment spreadsheet from [here]()
     """
-    PRE_PAYMENT_DATA = CSV.File(
-            DATADIR*"Staying_Put_grant_letter_to_LAs_2019-20_220319.csv",
-            delim=',' ) |> DataFrame
+    PRE_PAYMENT_DATA = get_payment_data( GlobalData.SIMULATION_YEAR )
+
+    function get_ofsted_data( year :: Integer ) :: DataFrame
+        ys = yearstr( year )
+        DATADIR*"Fostering_in_England__dataset_$(ys)_transposed_extended.csv",
+        delim=',',
+        missingstrings=["-999","",".."],
+        types=maketypeblock(3:1000)
+         ) |> DataFrame
+    end
 
     """
     This is my augmented version of the OFSTED fostering spreadsheet from [here](), with added info
     from the [AFC Fostering Study Spreadsheet]().
     """
-    PRE_OFDATA =  CSV.File(
-        DATADIR*"Fostering_in_England_2017-18_dataset_transposed_extended.csv",
-        delim=',',
-        missingstrings=["-999","",".."],
-        types=maketypeblock(3:1000)
-         ) |> DataFrame
-
-
+    PRE_OFDATA =  get_ofsted_data( GlobalData.SIMULATION_YEAR )
 
     function fixupccodes()
                n = size(LAMAPPINGS)[1]
@@ -62,30 +82,30 @@ module DataEditing
 
     returns - list of names that have not been mapped
     """
-    function addonscodesto!( data :: DataFrame, cname :: Symbol, ccode :: Symbol, rcode :: Symbol )
+    function add_ons_codes_to!( data :: DataFrame, cname :: Symbol, ccode :: Symbol, rcode :: Symbol )
         println( data[cname] )
         data[ccode]=map( c->codefromname(c), data[cname])
         data[rcode]=map( c->regioncodefromname(c), data[cname])
     end
 
 
-    function makeediteddatasets()
+    function make_edited_datasets()
         path = DATADIR*"edited/"
-        addonscodesto!( PRE_PAYMENT_DATA, :council, :ccode, :rcode )
+        add_ons_codes_to!( PRE_PAYMENT_DATA, :council, :ccode, :rcode )
         CSV.write( path*"PAYMENT_DATA.csv", PRE_PAYMENT_DATA )
-        addonscodesto!( PRE_OFDATA, :council, :ccode, :rcode )
+        add_ons_codes_to!( PRE_OFDATA, :council, :ccode, :rcode )
         CSV.write( path*"OFDATA.csv", PRE_OFDATA )
     end
 
-    function makeditedhbdata( year )
+    function make_edited_hb_data( year )
         # just for reference ..
         if year == 2019
             hbdata =  load( DATADIR*"2019-20_LHA_TABLES.csv" ) |> DataFrame
-            addonscodesto!( hbdata, :BRMA, :ccode, :rcode )
+            add_ons_codes_to!( hbdata, :BRMA, :ccode, :rcode )
             CSV.write( DATADIR*"edited/"*"LHA_2019.csv", hbdata )
         elseif year == 2020
             hbdata =  load( DATADIR*"202021_LHA_TABLES.csv" ) |> DataFrame
-            addonscodesto!( hbdata, :BRMA, :ccode, :rcode )
+            add_ons_codes_to!( hbdata, :BRMA, :ccode, :rcode )
             CSV.write( DATADIR*"edited/"*"LHA_2020.csv", hbdata )
         end
     end
